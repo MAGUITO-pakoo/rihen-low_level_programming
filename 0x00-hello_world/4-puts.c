ed77f9bde56df8dd2db642765d648c00233dc6c6
MAGUITO-pakoo <pakoorichtel@gmail.com>
2025-02-07 18:41:58 +0100

    deuxième modification

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  * main - Point d'entrée
+ *   *
+ *    * Return: Toujours 0 (Succès)
+ *     */
+int main(void)
+{
+	    puts("Programming is like building a multilingual puzzle");
+	        return (0);
+}
+
