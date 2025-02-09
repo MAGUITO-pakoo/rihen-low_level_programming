634c83e377d456377fe444d60b529b906490e539
MAGUITO-pakoo <pakoorichtel@gmail.com>
2025-02-09 22:11:39 +0100

    troisième modif

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,6 +2,19 @@
 
 int main(void)
 {
-	    int i, j;
+int n;
 
-	        for (i = 0; i < 9; i++) 					                   	                              }
+for (n = 0; n <= 9; n++)
+{
+putchar(n + '0');
+if (n < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+		        
+return (0);
+}
+
